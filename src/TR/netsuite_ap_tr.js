b8ee557e677a303927c0b57fa1b1be870f593b87
kazibizcloud <kazi.ali@bizcloudexperts.com>
2023-03-21 16:24:08 +0530

    business segment for TR M1

--- src/TR/netsuite_ap_tr.js
+++ src/TR/netsuite_ap_tr.js
@@ -14,6 +14,7 @@ const {
   triggerReportLambda,
   sendDevNotification,
 } = require("../../Helpers/helper");
+const { getBusinessSegment } = require("../../Helpers/businessSegmentHelper");
 
 let userConfig = "";
 let connections = "";
@@ -932,29 +933,7 @@ function getHardcodeData(isIntercompany = false) {
     finalizedbyInternalId: process.env.STAGE === "dev" ? "2511" : "2614", //prod:-2614  dev:-2511
     class: {
       head: "9",
-      line: {
-        international: 3,
-        domestic: 2,
-        warehouse: 16,
-        vas: 5,
-        sorting: 35,
-        "system level testing (slt)": 34,
-        "value added services": 33,
-        storage: 45,
-        wh1: 18,
-        wh2: 37,
-        wh3: 38,
-        wh4: 39,
-        "local delivery": 17,
-        air: 50,
-        "customs brokerage": 52,
-        expedite: 53,
-        "ground standard": 54,
-        ocean: 55,
-        "small pack": 56,
-        tl: 57,
-        "warehouse/vas": 58,
-      },
+      line: getBusinessSegment(process.env.STAGE),
     },
     department: {
       default: { head: "15", line: "2" },
