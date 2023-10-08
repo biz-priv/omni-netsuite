972372e48301641a7c10088d6aff5d5afe77d24a
Madhava mvs <VenkataSaiMadhava_Matta@neimanmarcus.com>
2023-10-08 14:51:58 +0530

    updated jenkins and removed events for cw and wt

--- src/WT_REST_API/netsuite_ar_wt.js
+++ src/WT_REST_API/netsuite_ar_wt.js
@@ -218,7 +218,7 @@ async function makeJsonPayload(data) {
       custbody9: singleItem.housebill_nbr ?? "",
       custbody17: singleItem.email ?? "",
       custbody25: singleItem.zip_code ?? "",
-      custbody29: singleItem.rfiemail ?? "",//dev :custbody29 prod: custbody27
+      custbody27: singleItem.rfiemail ?? "",//dev :custbody29 prod: custbody27
       item: data.map((e) => {
         return {
           // custcol_mfc_line_unique_key:"",
