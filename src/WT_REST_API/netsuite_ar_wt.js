52d1bfd1c7aebf1c44758836cf0d789d282ce5de
madhavamvs25 <madhava.matta@bizcloudexperts.com>
2024-02-13 20:15:04 +0530

    updated the custbody29 in dev

--- src/WT_REST_API/netsuite_ar_wt.js
+++ src/WT_REST_API/netsuite_ar_wt.js
@@ -223,7 +223,7 @@ async function makeJsonPayload(data) {
       custbody9: singleItem.housebill_nbr ?? "",
       custbody17: singleItem.email ?? "",
       custbody25: singleItem.zip_code ?? "",
-      custbody27: singleItem.rfiemail ?? "",//dev :custbody29 prod: custbody27
+      custbody29: singleItem.rfiemail ?? "",//dev :custbody29 prod: custbody27
       item: data.map((e) => {
         return {
           // custcol_mfc_line_unique_key:"",
